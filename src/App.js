78f9216495e90f67e72d413360f81e93e0ca0545
virensuthar <virensuthar5879@gmail.com>
2021-07-18 23:30:47 +0530

    upadated styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import SearchMovie from "./components/SearchMovie";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					React Movie Search App
-				</a>
-			</header>
+		<div>
+			<Navbar />
+			<SearchMovie />
 		</div>
 	);
 }
