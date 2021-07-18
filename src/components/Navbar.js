6f19a196beb6cb988b1fb7514103e01c31e67da4
virensuthar <virensuthar5879@gmail.com>
2021-07-18 23:28:46 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<div className="navbar">
+			<h1>
+				<i className="bi bi-tv"></i> MovieFind
+			</h1>
+			<div className="links">
+				<a href="/">About</a>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
